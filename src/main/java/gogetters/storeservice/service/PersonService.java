c04968a8fcabd4bcab19e6c9aa8cd38ed23dc43e
ullasnaik <naik.ullas@ymail.com>
2015-11-21 22:43:50 +0530

    test

---
+++ src/main/java/gogetters/storeservice/service/PersonService.java
@@ -0,0 +1,15 @@
+package gogetters.storeservice.service;
+
+import java.util.List;
+
+import gogetters.storeservice.model.Person;
+
+public interface PersonService {
+
+	public void addPerson(Person p);
+	public void updatePerson(Person p);
+	public List<Person> listPersons();
+	public Person getPersonById(int id);
+	public void removePerson(int id);
+	
+}
