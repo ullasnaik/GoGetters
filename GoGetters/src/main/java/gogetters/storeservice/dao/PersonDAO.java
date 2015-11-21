fb1a6ed92336fdc5ef90bac3c324785fc85bbe0f
ullasnaik <naik.ullas@ymail.com>
2015-11-21 19:44:59 +0530

    Go getters first commit

---
+++ GoGetters/src/main/java/gogetters/storeservice/dao/PersonDAO.java
@@ -0,0 +1,14 @@
+package gogetters.storeservice.dao;
+
+import java.util.List;
+
+import gogetters.storeservice.model.Person;
+
+public interface PersonDAO {
+
+	public void addPerson(Person p);
+	public void updatePerson(Person p);
+	public List<Person> listPersons();
+	public Person getPersonById(int id);
+	public void removePerson(int id);
+}
